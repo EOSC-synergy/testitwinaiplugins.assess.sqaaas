31f0980cf02d13eb2d170694e3b2cfee9b7dae08
samuelbernardolip <44866834+samuelbernardolip@users.noreply.github.com>
2025-08-08 08:25:56 +0100

    Add JePL file structure

--- .sqa/script.queasy-teal-discus.sh
+++ .sqa/script.queasy-teal-discus.sh
@@ -5,5 +5,7 @@
 
 (
 cd github.com/PalomoIFCA/testitwinaiplugins &&
-    pytest  --json-report  --json-report-indent 4
+    make torch-env-cpu&&
+    make tensorflow-env-cpu&&
+    .venv-pytorch/bin/pytest -v ./tests/ -m "not hpc and not memory_heavy"
 )
